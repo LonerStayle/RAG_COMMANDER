7f89b575acdd3d153b40d68e39e7f3644e348965
Loner <LonerStayle@github.com>
2025-10-24 18:03:46 +0900

    add

---
+++ src/tools/mcp/mcp_client.py
@@ -0,0 +1,62 @@
+from langchain_mcp_adapters import MultiServerMCPClient
+from dotenv import load_dotenv
+import os, platform
+
+load_dotenv()
+
+_client = None
+_tools = None
+
+
+def get_exa_config():
+    """Exa MCP 예시"""
+    MCP_KEY = os.getenv("MCP_KEY")
+
+
+    if platform.system() == "Windows":
+        return {
+            "command": "cmd",
+            "args": [
+                "/c",
+                "npx",
+                "-y",
+                "@smithery/cli@latest",
+                "run",
+                "exa",
+                "--key",
+                MCP_KEY,
+                "--profile",
+                "usual-reindeer-MZSQQr",
+            ],
+            "transport": "stdio",
+        }
+    else:
+        return {
+            "command": "npx",
+            "args": [
+                "-y",
+                "@smithery/cli@latest",
+                "run",
+                "exa",
+                "--key",
+                MCP_KEY,
+                "--profile",
+                "usual-reindeer-MZSQQr",
+            ],
+            "transport": "stdio",
+        }
+    
+async def get_client():
+    global _client
+    if _client is None: 
+        _client = MultiServerMCPClient(
+            {
+                "exa":get_exa_config()
+            }
+        )
+
+async def get_tools():
+    global _tools
+    if _tools is None:
+        client = await get_client()
+        
---
+++ src/utils/enum.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+
+
+class ModelName(StrEnum):
+    GPT_4_1_MINI = "gpt-4.1-mini"
+    GPT_4_1 = "gpt-4.1"
+    GPT_5_MINI = "gpt-5-mini"
+    GPT_5 = "gpt-5"
+    GPT_5_PRO = "gpt-5-pro"
+
+    CLAUDE_OPUS_4_1_20250805 = "claude-opus-4-1-20250805"
+    CLAUDE_SONNET_4_5_20250929 = "claude-sonnet-4-5-20250929"
