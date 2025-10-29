bd9280cbef7f38c16ce659bf40d25ede4a7ef4ba
Loner <LonerStayle@github.com>
2025-10-29 10:50:04 +0900

    add

---
+++ src/tools/rag/retriever/housing_faq_retriever.py
@@ -0,0 +1,20 @@
+from langchain_community.vectorstores import PGVector
+from langchain_openai import OpenAIEmbeddings
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+def retriever_housing_faq():
+    emb = OpenAIEmbeddings(model="text-embedding-3-small")
+    connection_url = os.getenv("POSTGRES_URL")
+
+    # 기존 collection 불러오기
+    collection_name = "text"
+    store = PGVector(
+        embedding_function=emb,
+        connection_string=connection_url,
+        collection_name=collection_name,
+        use_jsonb=True,
+    )
+    retriever = store.as_retriever(kwargs = {"k":5})
+    return retriever
