5011f49fa3eddc3194c86bd873d05968ba810258
hwain <immortal0900@gmail.com>
2025-10-30 00:14:59 +0900

    tool_two

--- src/lab/test.py
+++ src/lab/test.py
@@ -1,7 +1,10 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-from tools.estate_web_crowling import export_policy_factors
+import sys, pathlib
 
-saved_path = export_policy_factors(max_page=3)
-print(saved_path)
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
+
+from src.tools.Trade_Balance_tool import get_trade_balance
+
+if __name__ == "__main__":
+    result = get_trade_balance("서울")
+    
+    print(result)
